15f2418d6a38049a8bd028890d62c3229a5fe873
Steven Lou <slou50@stuy.edu>
2025-05-19 15:22:01 -0400

    Point class completed

---
+++ Point.java
@@ -0,0 +1,19 @@
+public class Point {
+  double x;
+  double y;
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+  public double getX() {
+    return this.x;
+  }
+  public double getY() {
+    return this.y;
+  }
+  public static void main(String[] args) {
+    Point dot = new Point(3.4, 3.5);
+    System.out.println(dot.getX());
+    System.out.println(dot.getY());
+  }
+}
