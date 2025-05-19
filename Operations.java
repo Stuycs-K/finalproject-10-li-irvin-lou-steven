1a259304ab75fd352c7790a36cba5b7cd1aa93b5
Irvin Li <irvinli@Irvins-Air.lan>
2025-05-19 19:57:55 -0400

    need to use try catch on arithmeticerror

--- Operations.java
+++ Operations.java
@@ -1,6 +1,17 @@
+import Point.java;
+
 public class Operations {
   public static void main(String[] args) {
 
   }
-  public static 
+  public static Point point_addition(Point Point1, Point Point2, int prime) {
+    if(Point1.getX().isInfinite() || Point1.getY().isInfinite()) {
+      return Point2;
+    }
+    if(Point2.getX().isInfinite() || Point2.getY().isInfinite()) {
+      return Point1;
+    }
+
+    if(P==Q_)
+  }
 }
