a35876e3df382643489c7acf183949bb71dfede5
Irvin Li <irvinli@Irvins-MacBook-Air.local>
2025-05-22 17:58:09 -0400

    making a new sign class in order to better format functions, outputs'

--- Signing.java
+++ Signing.java
@@ -1,3 +1,7 @@
+import java.math.BigInteger;
+
 public class Signing {
-    
+    public static Sign sign(String message, BigInteger private_key, Point InitialPoint, BigInteger order, BigInteger prime) {
+        return new Sign();
+    }
 }
