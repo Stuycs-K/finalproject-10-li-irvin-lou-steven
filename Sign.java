6e1351cbd5770b418e6e54f94b0bf9b04a9c1279
Steven Lou <slou50@stuy.edu>
2025-05-29 14:35:21 -0500

    almost done with verify signature

--- Sign.java
+++ Sign.java
@@ -8,9 +8,17 @@ public class Sign {
         this.message_hash = message_hash;
         this.signature = signature;
     }
-    
+
+    public String get_hash() {
+      return message_hash;
+    }
+
+    public Point get_signature() {
+      return signature;
+    }
+
     public String toString() {
         return "Signature: \n" + message_hash + "\n" + signature;
     }
-    
+
 }
--- Verify.java
+++ Verify.java
@@ -1,7 +1,28 @@
-public class Verify {
-    public static void main {
+import static java.nio.charset.StandardCharsets.UTF_8;
+import java.nio.charset.StandardCharsets;
+import java.math.BigInteger;
+import java.io.IOException;
 
+public class Verify {
+    public static void main(String[] args) {
+      System.out.println(verify(Signing.sign("IAMIRVIN.", new BigInteger("9813478"), Operations.initial, Operations.order, Operations.prime)));
     }
 
-    public static boolean verify(Sign signature, )
+    public static boolean verify(Sign signature, Point public_key, Point InitialPoint, BigInteger order, BigInteger prime) {
+      //u1 = hash * s^(-1) mod order
+      //u2 = r * s^(-1) mod order
+      //X = u1 * G + u2 * public
+      //Check X=r
+      BigInteger integer_hash = new BigInteger(signature.get_hash(), 16);
+      BigInteger z = integer_hash;
+      z = z.shiftRight(z.bitLength() - order.bitLength());
+      Point point_signature = signature.get_signature();
+      BigInteger r = point_signature.getX();
+      BigInteger s = point_signature.getY();
+      BigInteger s_inverse = s.modInverse(order);
+      BigInteger u1 = z.multiply(s_inverse).mod(order);
+      BigInteger u2 = r.multiply(s_inverse).mod(order);
+      Point result = Operations.point_multiplication(InitialPoint, u1, prime).add(Operations.point_multiplication(InitialPoint, u2, prime));
+      return result.getX().equals(r);
+    }
 }
